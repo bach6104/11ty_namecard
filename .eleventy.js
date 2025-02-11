f99b3ee95a4e61306cb221e0e680819fa21677c7
Admin <nguy1104@msu.edu>
2025-02-11 00:27:34 -0500

    finished website

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,15 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("styles"); 
-  eleventyConfig.addPassthroughCopy("scripts"); 
+  eleventyConfig.addPassthroughCopy("scripts");
 
-  return {
-    dir: {
-      input: ".", 
-      includes: "_includes", 
-      layouts: "_includes/layouts", 
-      output: "_site", 
-    },
-  };
+  // Shortcode for Planetary Cards
+  eleventyConfig.addShortcode("planet-cards", function (group, name, astroUnit, size, color) {
+    return `
+      <div class="card ${group}">
+        <div class="planet-avatar" style="width: ${size}; height: ${size}; background-color: ${color};"></div>
+        <h2>${name}</h2>
+        <h3>${astroUnit}</h3>
+      </div>
+    `;
+  });
 };
